39a2b04418c14e888b26224da74d0a48a346dae7
Cédric Marcone <cedric.marcone@valraiso.fr>
2019-04-08 11:03:07 +0200

    Exemple 6 (avec styled-jsx)

---
+++ src/App.6.js
@@ -0,0 +1,74 @@
+// Pour le linter je ne sais pas :
+// => chez nous, on ne variabilise pas les media queries
+
+// En revanche, on variabilise des propriétés "classiques".
+// => Et ça, ça fonctionne, exemple :
+// .delivery__prices-card {
+//  padding: 6px 12px;
+//  background-color: ${theme.primary.background};
+//}
+
+// En attendant, il faudrait désactiver cette règle de css-lint mais je n'ai pas le temps de checker.
+// => En clair, c'est un bug dans l'extension "styled-jsx-language-server" de vscode
+// Dans l'intervalle, tu peux ajouter dans les settings de vscode :
+// "css.validate": false
+
+// Pour le reste, j'aurais fait plutôt comme ça :
+
+import React from 'react';
+import classnames from 'classnames';
+import css from 'styled-jsx/css';
+
+const breakpoints = {
+  md: '960px',
+  lg: '1280px'
+};
+
+const styles = css`
+  .heading {
+    background: pink;
+    padding: 6px 12px;
+  }
+  .heading--left {
+    text-align: left;
+  }
+  .heading--center {
+    text-align: center;
+  }
+  .heading--right {
+    text-align: right;
+  }
+  @media (min-width: ${breakpoints.md}) {
+    .heading {
+      background: cadetblue;
+    }
+  }
+`;
+
+const Heading = ({ tag, align, children, ...props }) => {
+  const Tag = tag || 'h1';
+  const tagClasses = classnames('heading', props.className, {
+    'heading--left': align === 'left',
+    'heading--center': align === 'center',
+    'heading--right': align === 'right'
+  });
+  return (
+    <Tag className={tagClasses} {...props}>
+      {children}
+      <style jsx>{styles}</style>
+    </Tag>
+  );
+};
+
+const App = () => (
+  <div className="App">
+    <Heading tag="h2" align="center">
+      <span role="img" aria-label="wave">
+        👋
+      </span>
+      <span>Cédric</span>
+    </Heading>
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App1 from "./App.1";
-import App2 from "./App.2";
-import App3 from "./App.3";
-import App4 from "./App.4";
-import App5 from "./App.5";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App1 from './App.1';
+import App2 from './App.2';
+import App3 from './App.3';
+import App4 from './App.4';
+import App5 from './App.5';
+import App6 from './App.6';
 
-ReactDOM.render(<App1 />, document.getElementById("app1"));
-ReactDOM.render(<App2 />, document.getElementById("app2"));
-ReactDOM.render(<App3 />, document.getElementById("app3"));
-ReactDOM.render(<App4 />, document.getElementById("app4"));
-ReactDOM.render(<App5 />, document.getElementById("app5"));
+ReactDOM.render(<App1 />, document.getElementById('app1'));
+ReactDOM.render(<App2 />, document.getElementById('app2'));
+ReactDOM.render(<App3 />, document.getElementById('app3'));
+ReactDOM.render(<App4 />, document.getElementById('app4'));
+ReactDOM.render(<App5 />, document.getElementById('app5'));
+ReactDOM.render(<App6 />, document.getElementById('app6'));
